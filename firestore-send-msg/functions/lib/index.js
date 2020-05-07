6394c581a3d0c482f507ee1c9914fc5ab2dabf1f
Oleksandr Odinok <oleksandr.odinok@messagebird.com>
2020-05-07 16:07:47 +0200

    fix: use es5 as target output

--- firestore-send-msg/functions/lib/index.js
+++ firestore-send-msg/functions/lib/index.js
@@ -8,6 +8,33 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 var __importStar = (this && this.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
@@ -19,14 +46,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const admin = __importStar(require("firebase-admin"));
-const functions = __importStar(require("firebase-functions"));
-const messagebird_1 = __importDefault(require("messagebird"));
-const config_1 = __importDefault(require("./config"));
-const log_1 = require("./log");
-let db;
-let mb;
-let initialized = false;
+var admin = __importStar(require("firebase-admin"));
+var functions = __importStar(require("firebase-functions"));
+var messagebird_1 = __importDefault(require("messagebird"));
+var config_1 = __importDefault(require("./config"));
+var log_1 = require("./log");
+var db;
+var mb;
+var initialized = false;
 /**
  * Initializes Admin SDK & MessageBird client if not already initialized.
  */
@@ -43,118 +70,156 @@ function initialize() {
     log_1.logInfo("initialization finished successfuly");
 }
 function deliver(payload, ref) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const update = {
-            "delivery.attempts": admin.firestore.FieldValue.increment(1),
-            "delivery.endTime": admin.firestore.FieldValue.serverTimestamp(),
-            "delivery.error": null,
-            "delivery.leaseExpireTime": null,
-        };
-        log_1.logInfo("delivery attempt");
-        try {
-            if (!payload.channelId) {
-                throw new Error("Failed to deliver message. ChannelId is not defined.");
-            }
-            if (!payload.to) {
-                throw new Error("Failed to deliver message. Recipient of the message should be filled.");
-            }
-            if (!payload.content) {
-                throw new Error("Failed to deliver message. Message content is empty.");
-            }
-            log_1.logInfo(`sending message to channelId: ${payload.channelId}`);
-            log_1.logInfo(`with content:`, payload.content);
-            yield new Promise((resolve, reject) => {
-                mb.conversations.start(payload, function (err, response) {
-                    if (err) {
-                        log_1.logWarn(`send failed, got error: ${err}`);
-                        return reject(err);
+    return __awaiter(this, void 0, void 0, function () {
+        var update, e_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    update = {
+                        "delivery.attempts": admin.firestore.FieldValue.increment(1),
+                        "delivery.endTime": admin.firestore.FieldValue.serverTimestamp(),
+                        "delivery.error": null,
+                        "delivery.leaseExpireTime": null,
+                    };
+                    log_1.logInfo("delivery attempt");
+                    _a.label = 1;
+                case 1:
+                    _a.trys.push([1, 3, , 4]);
+                    if (!payload.channelId) {
+                        throw new Error("Failed to deliver message. ChannelId is not defined.");
                     }
-                    log_1.logInfo(`send successfully scheduled, got response: ${response}`);
-                    update["messageId"] = response.id;
-                    update["delivery.state"] = "SUCCESS";
-                    resolve();
-                });
-            });
-        }
-        catch (e) {
-            log_1.logInfo(`updating delivery record with error message`);
-            update["delivery.state"] = "ERROR";
-            update["delivery.error"] = e.toString();
-        }
-        return db.runTransaction((transaction) => {
-            transaction.update(ref, update);
-            return Promise.resolve();
+                    if (!payload.to) {
+                        throw new Error("Failed to deliver message. Recipient of the message should be filled.");
+                    }
+                    if (!payload.content) {
+                        throw new Error("Failed to deliver message. Message content is empty.");
+                    }
+                    log_1.logInfo("sending message to channelId: " + payload.channelId);
+                    log_1.logInfo("with content:", payload.content);
+                    return [4 /*yield*/, new Promise(function (resolve, reject) {
+                            mb.conversations.start(payload, function (err, response) {
+                                if (err) {
+                                    log_1.logWarn("send failed, got error: " + err);
+                                    return reject(err);
+                                }
+                                log_1.logInfo("send successfully scheduled, got response: " + response);
+                                update["messageId"] = response.id;
+                                update["delivery.state"] = "SUCCESS";
+                                resolve();
+                            });
+                        })];
+                case 2:
+                    _a.sent();
+                    return [3 /*break*/, 4];
+                case 3:
+                    e_1 = _a.sent();
+                    log_1.logInfo("updating delivery record with error message");
+                    update["delivery.state"] = "ERROR";
+                    update["delivery.error"] = e_1.toString();
+                    return [3 /*break*/, 4];
+                case 4: return [2 /*return*/, db.runTransaction(function (transaction) {
+                        transaction.update(ref, update);
+                        return Promise.resolve();
+                    })];
+            }
         });
     });
 }
 function processCreate(snap) {
-    return __awaiter(this, void 0, void 0, function* () {
-        log_1.logInfo("new msg added, init delivery object for it");
-        return db.runTransaction((transaction) => {
-            transaction.update(snap.ref, {
-                delivery: {
-                    startTime: admin.firestore.FieldValue.serverTimestamp(),
-                    state: "PENDING",
-                    attempts: 0,
-                    error: null,
-                },
-            });
-            return Promise.resolve();
+    return __awaiter(this, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            log_1.logInfo("new msg added, init delivery object for it");
+            return [2 /*return*/, db.runTransaction(function (transaction) {
+                    transaction.update(snap.ref, {
+                        delivery: {
+                            startTime: admin.firestore.FieldValue.serverTimestamp(),
+                            state: "PENDING",
+                            attempts: 0,
+                            error: null,
+                        },
+                    });
+                    return Promise.resolve();
+                })];
         });
     });
 }
 function processWrite(change) {
-    return __awaiter(this, void 0, void 0, function* () {
-        log_1.logInfo("processing write");
-        if (!change.after.exists) {
-            log_1.logInfo("ignoring delete");
-            return null;
-        }
-        if (!change.before.exists && change.after.exists) {
-            log_1.logInfo("process create");
-            return processCreate(change.after);
-        }
-        const payload = change.after.data();
-        log_1.logInfo("processing update");
-        switch (payload.delivery.state) {
-            case "SUCCESS":
-            case "ERROR":
-                log_1.logInfo("current state is SUCCESS/ERROR");
-                return null;
-            case "PROCESSING":
-                log_1.logInfo("current state is PROCESSING");
-                if (payload.delivery.leaseExpireTime.toMillis() < Date.now()) {
-                    return db.runTransaction((transaction) => {
-                        transaction.update(change.after.ref, {
-                            "delivery.state": "ERROR",
-                            error: "Message processing lease expired.",
-                        });
-                        return Promise.resolve();
-                    });
-                }
-                return null;
-            case "PENDING":
-            case "RETRY":
-                log_1.logInfo("current state is PENDING/RETRY");
-                yield db.runTransaction((transaction) => {
-                    transaction.update(change.after.ref, {
-                        "delivery.state": "PROCESSING",
-                        "delivery.leaseExpireTime": admin.firestore.Timestamp.fromMillis(Date.now() + 60000),
-                    });
-                    return Promise.resolve();
-                });
-                log_1.logInfo("record set to PROCESSING state, trying to deliver the message");
-                return deliver(payload, change.after.ref);
-        }
+    return __awaiter(this, void 0, void 0, function () {
+        var payload, _a;
+        return __generator(this, function (_b) {
+            switch (_b.label) {
+                case 0:
+                    log_1.logInfo("processing write");
+                    if (!change.after.exists) {
+                        log_1.logInfo("ignoring delete");
+                        return [2 /*return*/, null];
+                    }
+                    if (!change.before.exists && change.after.exists) {
+                        log_1.logInfo("process create");
+                        return [2 /*return*/, processCreate(change.after)];
+                    }
+                    payload = change.after.data();
+                    log_1.logInfo("processing update");
+                    _a = payload.delivery.state;
+                    switch (_a) {
+                        case "SUCCESS": return [3 /*break*/, 1];
+                        case "ERROR": return [3 /*break*/, 1];
+                        case "PROCESSING": return [3 /*break*/, 2];
+                        case "PENDING": return [3 /*break*/, 3];
+                        case "RETRY": return [3 /*break*/, 3];
+                    }
+                    return [3 /*break*/, 5];
+                case 1:
+                    log_1.logInfo("current state is SUCCESS/ERROR");
+                    return [2 /*return*/, null];
+                case 2:
+                    log_1.logInfo("current state is PROCESSING");
+                    if (payload.delivery.leaseExpireTime.toMillis() < Date.now()) {
+                        return [2 /*return*/, db.runTransaction(function (transaction) {
+                                transaction.update(change.after.ref, {
+                                    "delivery.state": "ERROR",
+                                    error: "Message processing lease expired.",
+                                });
+                                return Promise.resolve();
+                            })];
+                    }
+                    return [2 /*return*/, null];
+                case 3:
+                    log_1.logInfo("current state is PENDING/RETRY");
+                    return [4 /*yield*/, db.runTransaction(function (transaction) {
+                            transaction.update(change.after.ref, {
+                                "delivery.state": "PROCESSING",
+                                "delivery.leaseExpireTime": admin.firestore.Timestamp.fromMillis(Date.now() + 60000),
+                            });
+                            return Promise.resolve();
+                        })];
+                case 4:
+                    _b.sent();
+                    log_1.logInfo("record set to PROCESSING state, trying to deliver the message");
+                    return [2 /*return*/, deliver(payload, change.after.ref)];
+                case 5: return [2 /*return*/];
+            }
+        });
     });
 }
-exports.processQueue = functions.handler.firestore.document.onWrite((change) => __awaiter(void 0, void 0, void 0, function* () {
-    initialize();
-    try {
-        yield processWrite(change);
-    }
-    catch (err) {
-        log_1.logWarn("unexpected error during execution: ", err);
-        return null;
-    }
-}));
+exports.processQueue = functions.handler.firestore.document.onWrite(function (change) { return __awaiter(void 0, void 0, void 0, function () {
+    var err_1;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                initialize();
+                _a.label = 1;
+            case 1:
+                _a.trys.push([1, 3, , 4]);
+                return [4 /*yield*/, processWrite(change)];
+            case 2:
+                _a.sent();
+                return [3 /*break*/, 4];
+            case 3:
+                err_1 = _a.sent();
+                log_1.logWarn("unexpected error during execution: ", err_1);
+                return [2 /*return*/, null];
+            case 4: return [2 /*return*/];
+        }
+    });
+}); });
--- firestore-send-msg/functions/lib/log.js
+++ firestore-send-msg/functions/lib/log.js
@@ -1,18 +1,33 @@
 "use strict";
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const config_1 = __importDefault(require("./config"));
-function logInfo(msg, ...rest) {
+var config_1 = __importDefault(require("./config"));
+function logInfo(msg) {
+    var rest = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        rest[_i - 1] = arguments[_i];
+    }
     if (msg && config_1.default.logLevel === "info") {
-        console.info(`INFO: firestore-send-msg: ${msg}`, ...rest);
+        console.info.apply(console, __spreadArrays(["INFO: firestore-send-msg: " + msg], rest));
     }
 }
 exports.logInfo = logInfo;
-function logWarn(msg, ...rest) {
+function logWarn(msg) {
+    var rest = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        rest[_i - 1] = arguments[_i];
+    }
     if (msg && (config_1.default.logLevel === "warn" || config_1.default.logLevel === "info")) {
-        console.warn(`WARN: firestore-send-msg: ${msg}`, ...rest);
+        console.warn.apply(console, __spreadArrays(["WARN: firestore-send-msg: " + msg], rest));
     }
 }
 exports.logWarn = logWarn;
