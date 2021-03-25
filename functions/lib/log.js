e1ec8fa2e09a3e1929d5aefa344b0343d2444e14
Oleksandr Odinok <oleksandr.odinok@messagebird.com>
2021-03-25 20:46:41 +0100

    fix: #17 use firebase logger instead of console

--- functions/lib/log.js
+++ functions/lib/log.js
@@ -4,16 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.logWarn = exports.logInfo = void 0;
+const firebase_functions_1 = require("firebase-functions");
 const config_1 = __importDefault(require("./config"));
 function logInfo(msg, ...rest) {
     if (msg && config_1.default.logLevel === "info") {
-        console.info(`INFO: firestore-messagebird-send-msg: ${msg}`, ...rest);
+        firebase_functions_1.logger.info(`INFO: firestore-messagebird-send-msg: ${msg}`, ...rest);
     }
 }
 exports.logInfo = logInfo;
 function logWarn(msg, ...rest) {
     if (msg && (config_1.default.logLevel === "warn" || config_1.default.logLevel === "info")) {
-        console.warn(`WARN: firestore-messagebird-send-msg: ${msg}`, ...rest);
+        firebase_functions_1.logger.warn(`WARN: firestore-messagebird-send-msg: ${msg}`, ...rest);
     }
 }
 exports.logWarn = logWarn;
--- functions/src/log.ts
+++ functions/src/log.ts
@@ -1,13 +1,15 @@
+import { logger } from "firebase-functions";
+
 import config from "./config";
 
 export function logInfo(msg: string, ...rest) {
   if (msg && config.logLevel === "info") {
-    console.info(`INFO: firestore-messagebird-send-msg: ${msg}`, ...rest);
+    logger.info(`INFO: firestore-messagebird-send-msg: ${msg}`, ...rest);
   }
 }
 
 export function logWarn(msg: string, ...rest) {
   if (msg && (config.logLevel === "warn" || config.logLevel === "info")) {
-    console.warn(`WARN: firestore-messagebird-send-msg: ${msg}`, ...rest);
+    logger.warn(`WARN: firestore-messagebird-send-msg: ${msg}`, ...rest);
   }
 }
